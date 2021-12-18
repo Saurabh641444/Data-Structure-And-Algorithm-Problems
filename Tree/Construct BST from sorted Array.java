49951d571ba0e57fae264287813733a1e442673c
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2021-12-19 00:20:59 +0530

    Create Construct BST from sorted Array.java

---
+++ Tree/Construct BST from sorted Array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return helper(nums,0,nums.length-1);
+    }
+
+    private TreeNode helper(int[] nums,int l, int r){
+
+    if(l>r) return null;
+    if(l==r) return new TreeNode(num[l]);
+     
+    int mid=(l+r)/2;
+    TreeNode root=new TreeNode(nums[1]);
+
+    root.left=helper(nums,l,mid-1);
+    root.right=helper(nums,mid+1,r);
+    return root;
+}
+}
