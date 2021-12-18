732b7d7e8af9c755814f74f58302dffcaff104aa
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2021-12-19 00:20:29 +0530

    Create Construct the binary tree from pre order traversal.java

---
+++ Tree/Construct the binary tree from pre order traversal.java
@@ -0,0 +1,23 @@
+class Solution {
+    public TreeNode bstFromPreorder(int[] preorder) {
+        
+   return helper(preorder,0,preorder.length);
+    }
+
+   public TreeNode helper(int[] preorder, int rootindex,int right){
+        if(rootindex>=right) return null;//adding null in tree
+        
+     int value=preorder[rootindex];
+     TreeNode root=new TreeNode(value);
+
+     int i=rootindex+1;
+     while(i<=preorder.length-1&&preorder[i] <value){ i++;}// this loop is for dividing the treee
+         
+     root.left=helper(preorder,rootindex+1,i);
+     root.right=helper(preorder,i,right);
+
+     return root;
+        
+}    
+
+}
