86d24640cd1a647b3f82b43859e570e573e855f7
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-01-09 07:25:07 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (5.18%) - LeetHub

--- 66-plus-one/66-plus-one.java
+++ 66-plus-one/66-plus-one.java
@@ -1,16 +1,44 @@
+// class Solution {
+//     public int[] plusOne(int[] digits) {
+//         int n=digits.length;
+//        int carry=1;
+    
+//        for(int i=n-1;i>=0;i--){
+//        digits[i]+=carry;
+//        if(digits[i]<=9){ return digits;}
+//        digits[i]=0;
+    
+//               }
+//     int[] res =new int[n+1];
+//     res[0]=1;
+//     return res;
+//      }
+// }
+
 class Solution {
     public int[] plusOne(int[] digits) {
-        int n=digits.length;
-       int carry=1;
-    
-       for(int i=n-1;i>=0;i--){
-       digits[i]+=carry;
-       if(digits[i]<=9){ return digits;}
-       digits[i]=0;
-    
-              }
-    int[] res =new int[n+1];
-    res[0]=1;
-    return res;
-     }
+        int len = digits.length; // length of array;
+        
+        // start the loop from last index;
+        for (int i = len-1; i>=0; i--) {
+            // if ith index is 9, that means there will be an overflow if we add 1
+            // adding 1 will make it 10, so we set it to 0. and continue the loop; 
+            if (digits[i] == 9) {
+                digits[i] = 0;
+            }else {
+                // if ith value is not 9 we just add 1 to it and return;
+                digits[i] += 1;
+                return digits;
+            }     
+        }
+        
+        // if we are here, that means all the value of the array were 9;
+        // and they are 0 now;
+        // example: [9,9,9,9], after loop becomes [0,0,0,0];
+        // so we have to create a new array of +1 length of the previous.
+        // and place 1 at 0th index;
+        int[] arr = new int[len+1];
+        arr[0] = 1;
+        return arr;
+    }
 }
