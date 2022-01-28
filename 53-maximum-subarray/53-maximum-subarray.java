5d1b6f6bc9a1852c839ff087476f1b2d200a2111
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-01-28 13:41:54 +0530

    Time: 1 ms (100.00%), Space: 51.6 MB (51.51%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -1,15 +1,36 @@
-class Solution {
-    public int maxSubArray(int[] nums) {
-        int sum=0;
-        int maximum=nums[0];
+// class Solution {
+//     public int maxSubArray(int[] nums) {
+//         int sum=0;
+//         int maximum=nums[0];
         
-        for(int num:nums){
-            sum+=num;
-               maximum=Math.max(sum,maximum);
+//         for(int num:nums){
+//             sum+=num;
+//                maximum=Math.max(sum,maximum);
         
-        if(sum<0){sum=0;}
-        }
+//         if(sum<0){sum=0;}
+//         }
      
-        return maximum;
+//         return maximum;
+//     }
+// }
+
+class Solution {
+//solution by Kadane's algorithm
+    public int maxSubArray(int[] nums) {
+        if(nums.length==0) return 0; 
+        if(nums.length==1)  return nums[0];
+
+        int local_max=0;
+        int global_max=Integer.MIN_VALUE;
+       
+        for(int i=0; i<nums.length;i++){
+        local_max=Math.max(nums[i],nums[i]+local_max);
+
+         if(global_max<local_max){
+            global_max=local_max;
+            }
+          }
+        
+        return global_max;
     }
 }
