bb9a8e651f80802d47ded59ed5de976a6f1e1451
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-01-29 10:18:48 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
@@ -46,6 +46,33 @@ class gfg
 
 
 
+// class Solution 
+// { 
+    
+//     //Function to return max value that can be put in knapsack of capacity W.
+//     static int knapSack(int W, int wt[], int val[], int N) 
+//     {        // code here
+//         int[][] t=new int[N+1][W+1];
+//         for(int i=0;i<=N;i++){ t[i][0]=0;}
+//             for(int i=1;i<=W;i++){
+              
+//                 t[0][i]=0;
+//             }
+        
+//         for(int i=1;i<=N;i++){
+//             for(int j=1;j<=W;j++){
+//                 if(wt[i-1]<=j){
+//                     t[i][j]=Math.max(val[i-1]+t[i-1][j-wt[i-1]], t[i-1][j]);
+//                 }
+//                 else{
+//                     t[i][j]=t[i-1][j];
+//                 }
+//             }
+//         }
+//         return t[N][W];
+//     } 
+// }
+
 class Solution 
 { 
     
@@ -53,22 +80,28 @@ class Solution
     static int knapSack(int W, int wt[], int val[], int N) 
     {        // code here
         int[][] t=new int[N+1][W+1];
-        for(int i=0;i<=N;i++){ t[i][0]=0;}
-            for(int i=1;i<=W;i++){
+        for(int i=0;i<=N;i++){ 
+            for(int j=0;j<=W;j++){
               
-                t[0][i]=0;
-            }
-        
-        for(int i=1;i<=N;i++){
-            for(int j=1;j<=W;j++){
-                if(wt[i-1]<=j){
-                    t[i][j]=Math.max(val[i-1]+t[i-1][j-wt[i-1]], t[i-1][j]);
-                }
-                else{
-                    t[i][j]=t[i-1][j];
-                }
-            }
-        }
-        return t[N][W];
+                t[i][j]=-1;
+            }}
+        return Memo(W,wt,val,N,t);
     } 
+   
+ static int Memo(int W,int wt[],int val[],int N, int t[][]){
+      if(N==0||W==0) return 0;
+
+     if(t[N][W]!=-1){
+       return t[N][W];
+        }
+       
+    if(wt[N-1]<=W){
+          return t[N][W]=Math.max(val[N-1]+Memo(W-wt[N-1],wt,val,N-1,t),Memo(W,wt,val,N-1,t));
+     }
+else{
+    return t[N][W]=Memo(W,wt,val,N-1,t);
+   }
+
+    }
+   
 }
