92f1aa4020bfe546480cda95d23aab64294f391b
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-02-03 22:43:44 +0530

    Time: 288 ms (11.20%), Space: 57.1 MB (26.73%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.java
+++ 454-4sum-ii/454-4sum-ii.java
@@ -20,3 +20,20 @@ class Solution {
        return count;// 2O(N^2) ,space O(N^2)
     }
 }
+
+// class Solution {
+//     public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+//        int count=0;
+//        for(int a :nums1){
+//        for(int b:nums2){
+//        for(int c:nums3){
+//         for(int d:nums4){
+//            if(a+b+c+d==0){
+//                count++;}
+//              }
+//           }
+//      }        
+// }
+//        return count;// 2O(N^4) ,space O(1)
+//     }
+// }
