4b84217a3e7d8a7ce882575baf81d4d782ec825c
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-02-07 15:26:49 +0530

    Time: 2 ms (75.61%), Space: 42.2 MB (12.12%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.java
@@ -0,0 +1,10 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        char res=t.charAt(s.length());
+        for(int i=0;i<s.length();i++){
+            res^=s.charAt(i);
+            res^=t.charAt(i);
+        }
+        return res;
+    }
+}
