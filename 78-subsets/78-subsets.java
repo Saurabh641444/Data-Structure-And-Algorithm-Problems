059e413cb097fe3e41ae6310777b8a240751007c
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-02-13 14:35:44 +0530

    Time: 2 ms (44.88%), Space: 43.3 MB (13.19%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,22 +1,47 @@
+// class Solution {
+//     static List<List<Integer>> lists;
+//     public List<List<Integer>> subsets(int[] nums) {
+//         lists =new ArrayList<>();
+//         List<Integer> list=new ArrayList<>();
+//         solve(list,nums,0);
+//         return lists;
+//     }
+    
+//    public static void solve(List<Integer> list,int[] nums,int curr){
+         
+//          if(curr==nums.length){
+//                  lists.add(new ArrayList(list));
+//                   return;
+//                   }
+           
+//          List<Integer> list2=new ArrayList<>(list);
+//          list.add(nums[curr]);
+//          solve(list2,nums,curr+1);
+//          solve(list,nums,curr+1);
+//         }
+// }
+
 class Solution {
-    static List<List<Integer>> lists;
+     
     public List<List<Integer>> subsets(int[] nums) {
-        lists =new ArrayList<>();
-        List<Integer> list=new ArrayList<>();
-        solve(list,nums,0);
-        return lists;
+       
+        List<Integer> curr=new ArrayList<Integer>();
+        List<List<Integer>> ans=new ArrayList<>();
+        dfs(nums,0,curr,ans);
+        return ans;
     }
     
-   public static void solve(List<Integer> list,int[] nums,int curr){
+   public static void dfs(int[] arr,int i,List<Integer> curr,List<List<Integer>> ans){
          
-         if(curr==nums.length){
-                 lists.add(new ArrayList(list));
+         if(i==arr.length){
+                 ans.add(new ArrayList<Integer>(curr));
                   return;
                   }
            
-         List<Integer> list2=new ArrayList<>(list);
-         list.add(nums[curr]);
-         solve(list2,nums,curr+1);
-         solve(list,nums,curr+1);
+         
+         curr.add(arr[i]);
+         dfs(arr,i+1,curr,ans);
+         curr.remove(curr.size()-1);
+         dfs(arr,i+1,curr,ans);
         }
 }
