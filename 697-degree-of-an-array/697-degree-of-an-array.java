b45510338c769f80cc321c932376e26d5606f96d
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-02-15 18:27:37 +0530

    Time: 35 ms (41.98%), Space: 53.6 MB (28.92%) - LeetHub

--- 697-degree-of-an-array/697-degree-of-an-array.java
+++ 697-degree-of-an-array/697-degree-of-an-array.java
@@ -22,3 +22,37 @@ class Solution {
         return minLength;
     }
 }
+
+
+
+// class Solution
+// {
+// public int findShortestSubArray(int[] nums)
+// {
+// HashMap<Integer,Integer> freCount=new HashMap<>();//for counting the frequency of particular element
+// int maxDegree=0;//maximum frequency of element
+
+//     HashMap<Integer,Integer> firstIndex=new HashMap<>();//for storing the first index of maximum frequency to get minimum subarray
+//     int minLength=0;//minimum length of subarray that contins the element with maximum degree
+    
+//     for(int i=0;i<nums.length;i++)//traversing the nums array
+//     {
+//         firstIndex.putIfAbsent(nums[i],i);//putIfAbsent stores in the map if the key is not present//actually it stores the firstv index in a map format 
+            
+//         freCount.put(nums[i], freCount.getOrDefault(nums[i],0)+1);//counting  the the frequency of element //detOrDefault returns the default value if the key is not present, it assotiates with its default value//here it is used to to store the frequency of every element in the array
+        
+//         if(freCount.get(nums[i])>maxDegree)//getting the the maximum frequency or the degree of the array 
+//         {
+//             maxDegree=freCount.get(nums[i]);//storing the maximum degree by comparing with the corresponding key->value in the HashMap
+            
+//             minLength=i-firstIndex.get(nums[i])+1;//storing the minimum length for the element with max degrees 
+//         }
+//         else if(freCount.get(nums[i])==maxDegree)//if the degree is same getting the minimum length
+//         {
+//             minLength=Math.min(minLength,i-firstIndex.get(nums[i])+1);//getting the minimum length with previous and current length  
+//         }
+//     }
+//     return minLength;//returning the minimum length 
+// }
+// '''
+// }
