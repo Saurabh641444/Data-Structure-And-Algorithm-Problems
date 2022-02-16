1ec2ddf44a8f20fa95736b561c1c381474de78b1
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-02-16 20:22:18 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (9.06%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.java
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.java
@@ -8,19 +8,45 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode swapPairs(ListNode head) {
+   
+//     ListNode dummy=new ListNode();
+//     dummy.next=head;
+   
+        
+//     swap(dummy);
+//     return dummy.next;
+//     }
+
+//     void swap(ListNode node){
+//       if(node==null){ return;}
+      
+//       ListNode first=node.next;
+//       ListNode second=null;
+//       if(first!=null){
+//       second=first.next;
+//         }
+
+//       if(second!=null){
+//          ListNode secondNext=second.next;
+//          second.next=first;
+//          node.next=second;
+//          first.next=secondNext;
+//          swap(first);
+//          }
+//     }
+// }
+
 class Solution {
     public ListNode swapPairs(ListNode head) {
    
     ListNode dummy=new ListNode();
     dummy.next=head;
-    swap(dummy);
-    return dummy.next;
-    }
-
-    void swap(ListNode node){
-      if(node==null){ return;}
-      
-      ListNode first=node.next;
+    ListNode node=dummy;
+        
+    while(node!=null){
+         ListNode first=node.next;
       ListNode second=null;
       if(first!=null){
       second=first.next;
@@ -31,7 +57,18 @@ class Solution {
          second.next=first;
          node.next=second;
          first.next=secondNext;
-         swap(first);
-         }
+         // swap(first);
+          node=first;
+         }else{ break;}
     }
+        
+    // swap(dummy);
+    return dummy.next;
+    }
+
+//     void swap(ListNode node){
+//       if(node==null){ return;}
+      
+     
+//     }
 }
