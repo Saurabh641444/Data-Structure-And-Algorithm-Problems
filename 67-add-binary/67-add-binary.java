9e3bd224ccb87e394f856394c0a5147e2e7d6bb6
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-03-02 22:31:51 +0530

    Time: 14 ms (8.06%), Space: 43.2 MB (8.39%) - LeetHub

--- 67-add-binary/67-add-binary.java
+++ 67-add-binary/67-add-binary.java
@@ -1,23 +1,23 @@
 class Solution {
     public String addBinary(String a, String b) {
-    int carry=0;
+    int carry=0;// let's consider carry is zero first
     String result="";
-    int i=0;
+    int i=0;// Lets consider pointer
     int alen=a.length();
     int blen=b.length();
-    while(i<alen||i<blen||carry!=0){
+    while(i<alen||i<blen||carry!=0){//traverse through string
         int x=0;
         if(i<alen && a.charAt(alen-1-i)=='1') x=1;
         int y=0;
         if(i<blen && b.charAt(blen-1-i)=='1') y=1;
 
-        result=(x+y+carry)%2+result;
-        carry=(x+y+carry)/2;
+        result=(x+y+carry)%2+result;// do the addition
+        carry=(x+y+carry)/2;//find out the carry
         i++;
         
     }
     
-    return result;
+    return result;// return result
         
     }
 }
