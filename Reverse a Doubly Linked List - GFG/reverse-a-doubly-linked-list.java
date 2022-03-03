48b34c3d2dcbc2ebcb679b34251f07d9dc79815e
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-03-03 23:25:45 +0530

    Added solution - LeetHub

--- Reverse a Doubly Linked List - GFG/reverse-a-doubly-linked-list.java
+++ Reverse a Doubly Linked List - GFG/reverse-a-doubly-linked-list.java
@@ -90,17 +90,31 @@ class Node
 public static Node reverseDLL(Node  head)
 {
     //Your code here
-    if(head==null || head.next==null){ return head;}
-    Node prev=null;
-    Node curr=head;
-    while(curr!=null){
-        prev=curr.prev;
-        curr.prev=curr.next;
-        curr.next=prev;
-        curr=curr.prev;
-    }
+    // if(head==null || head.next==null){ return head;}
+    // Node prev=null;
+    // Node curr=head;
+    // while(curr!=null){
+    //     prev=curr.prev;
+    //     curr.prev=curr.next;
+    //     curr.next=prev;
+    //     curr=curr.prev;
+    // }
     
-    return prev.prev;
+    // return prev.prev;
+    
+    Node p=head;
+    Node q=head;
+    
+    while(q != null){
+        p=q;
+        
+        Node temp=q.prev;
+        q.prev=q.next;
+        q.next=temp;
+        
+        q=q.prev;
+    }
+    return p;
 }
 
 
