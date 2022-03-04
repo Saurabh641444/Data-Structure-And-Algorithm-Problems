775453f9cf06f5fa5204f1994f5b19694d719211
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-03-05 00:38:47 +0530

    Time: 1 ms (91.96%), Space: 43.3 MB (25.04%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.java
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.java
@@ -3,8 +3,11 @@ class Solution {
         int sum=0;
         
         for(int i=0;i<operations.length;i++){
-            if(operations[i].charAt(1)=='+') sum++;
-            else{ sum--;}
+            // if(operations[i].charAt(1)=='+') sum++;
+            // else{ sum--;}
+            String str=operations[i];
+            if(str.equals("X++") || str.equals("++X")){sum++;}
+            else{sum--;}
         }
         return sum;
     }
