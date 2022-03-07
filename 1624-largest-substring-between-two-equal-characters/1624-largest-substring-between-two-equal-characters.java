81d34d74b39b7b535bb65a46adc987934d0d00b8
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-03-07 11:47:57 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (36.16%) - LeetHub

--- 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.java
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.java
@@ -1,17 +1,30 @@
+// class Solution {
+//     public int maxLengthBetweenEqualCharacters(String s) {
+        
+//         HashMap<Character,Integer> map=new HashMap<Character,Integer>();
+//         int max=-1;
+        
+//         for(int i=0;i<s.length();i++){
+//             char c=s.charAt(i);
+//             if(map.containsKey(c)){
+//                 max=Math.max(max,i-map.get(c)-1);
+//             }else{
+//                 map.put(c,i);
+//             }
+//         }
+//         return max;
+//     }
+// }
+
 class Solution {
     public int maxLengthBetweenEqualCharacters(String s) {
+    
+        int maxLen=0;
         
-        HashMap<Character,Integer> map=new HashMap<Character,Integer>();
-        int max=-1;
-        
-        for(int i=0;i<s.length();i++){
-            char c=s.charAt(i);
-            if(map.containsKey(c)){
-                max=Math.max(max,i-map.get(c)-1);
-            }else{
-                map.put(c,i);
-            }
+        for(char i='a';i<='z';i++){
+            maxLen=Math.max(maxLen,s.lastIndexOf(i)-s.indexOf(i));
         }
-        return max;
+        
+        return maxLen==0 ? -1:maxLen-1;
     }
 }
