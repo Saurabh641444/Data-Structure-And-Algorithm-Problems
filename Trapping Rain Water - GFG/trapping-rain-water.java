8a5a9a09004dbc8f1f11c24b7f7835ecc8102d81
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-03-12 17:03:18 +0530

    Added solution - LeetHub

---
+++ Trapping Rain Water - GFG/trapping-rain-water.java
@@ -0,0 +1,66 @@
+// { Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+
+class Array {
+
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		while(t-->0){
+		  
+		    //size of array
+		    int n = Integer.parseInt(br.readLine().trim());
+		    int arr[] = new int[n];
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    
+		    //adding elements to the array
+		    for(int i=0; i<n; i++){
+		        arr[i] = Integer.parseInt(inputLine[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    
+		    //calling trappingWater() function
+		    System.out.println(obj.trappingWater(arr, n));
+		}
+	}
+}
+
+// } Driver Code Ends
+
+
+class Solution{
+    
+    // arr: input array
+    // n: size of array
+    // Function to find the trapped water between the blocks.
+    static long trappingWater(int arr[], int n) { 
+        // Your code here
+        int mxL[]=new int[n];
+        int mxR[]=new int[n];
+        
+        mxL[0]=arr[0];
+        mxR[n-1]=arr[n-1];
+        
+        for(int i=1;i<n;i++){
+            mxL[i]=Math.max(arr[i],mxL[i-1]);
+        }
+        
+        for(int i=n-2;i>=0;i--){
+            mxR[i]=Math.max(arr[i],mxR[i+1]);
+        }
+        long sum=0;
+        int[] water=new int[n];
+        for(int i=0;i<n;i++){
+          water[i]=Math.min(mxL[i],mxR[i])-arr[i];
+          sum+=water[i];
+        }
+        
+        return sum;
+    } 
+}
+
+
