bfafcccef78b6e936c2f5fec7838b5b0a4a968ae
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-03-14 14:54:00 +0530

    Time: 12 ms (96.17%), Space: 50.8 MB (58.63%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -1,25 +1,57 @@
+// class Solution {
+//     public String minRemoveToMakeValid(String s) {
+//         Stack<Integer> stack=new Stack();
+//         char ch[]=s.toCharArray();
+//         for(int i=0;i<ch.length;i++){
+//                if(ch[i]=='('){ stack.push(i);}
+//                else if(ch[i]==')'){
+//                         if(stack.isEmpty()){
+//                           ch[i]='*';  
+//                          }else{
+//                             stack.pop();
+//                                 }
+//                      }
+//               }
+//               while(!stack.isEmpty()){
+//                    ch[stack.pop()]='*';
+//                     }
+
+//            StringBuilder sb=new StringBuilder();
+//           for(int i=0;i<ch.length;i++){
+//               if(ch[i]!='*'){
+//                  sb.append(ch[i]);
+//                     }
+//                  }
+//            return sb.toString();
+//     }
+// }
+
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        Stack<Integer> stack=new Stack();
+        int open=0;
         char ch[]=s.toCharArray();
         for(int i=0;i<ch.length;i++){
-               if(ch[i]=='('){ stack.push(i);}
+               if(ch[i]=='('){ open++;}
                else if(ch[i]==')'){
-                        if(stack.isEmpty()){
-                          ch[i]='*';  
+                        if(open==0){
+                         ch[i]='*';
                          }else{
-                            stack.pop();
+                            open--;
                                 }
                      }
               }
-              while(!stack.isEmpty()){
-                   ch[stack.pop()]='*';
+              for(int i=ch.length-1;i>=0 ;i--){
+                      if(ch[i]=='('&& open>0){
+                           ch[i]='*';
+                            open--;
+                        }
                     }
 
            StringBuilder sb=new StringBuilder();
           for(int i=0;i<ch.length;i++){
               if(ch[i]!='*'){
                  sb.append(ch[i]);
+              
                     }
                  }
            return sb.toString();
