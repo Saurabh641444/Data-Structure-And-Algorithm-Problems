aa5dc3eb02a1da5616608e5741d87a472a4edb02
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-03-19 11:56:06 +0530

    Time: 47 ms (61.82%), Space: 67.4 MB (70.33%) - LeetHub

--- 895-maximum-frequency-stack/895-maximum-frequency-stack.java
+++ 895-maximum-frequency-stack/895-maximum-frequency-stack.java
@@ -1,22 +1,50 @@
+// class FreqStack {
+     
+//     private Map<Integer,Integer> freq;
+//     private TreeMap<Integer,Stack<Integer>> fs;
+//     public FreqStack() {
+//         freq=new HashMap<>();
+//         fs=new TreeMap<>();
+//     }
+    
+//     public void push(int x) {
+//        freq.put(x,freq.getOrDefault(x,0)+1);
+//        fs.computeIfAbsent(freq.get(x),s->new Stack<>()).add(x);    
+//     }
+    
+//     public int pop() {
+//         int lastKey=fs.lastKey();
+//         int x=fs.get(lastKey).pop();
+//         if(fs.get(lastKey).isEmpty()){
+//             fs.remove(lastKey);
+//          }
+//         freq.put(x,freq.getOrDefault(x,0)-1);
+//          return x;//O(logN)
+//     }
+// }
+
 class FreqStack {
      
     private Map<Integer,Integer> freq;
-    private TreeMap<Integer,Stack<Integer>> fs;
+    private Map<Integer,Stack<Integer>> fs;
+     private int maxFreq;
     public FreqStack() {
         freq=new HashMap<>();
-        fs=new TreeMap<>();
+        fs=new HashMap<>();
+        maxFreq=0;
     }
     
     public void push(int x) {
        freq.put(x,freq.getOrDefault(x,0)+1);
        fs.computeIfAbsent(freq.get(x),s->new Stack<>()).add(x);    
+       maxFreq=Math.max(maxFreq,freq.get(x));
     }
     
     public int pop() {
-        int lastKey=fs.lastKey();
-        int x=fs.get(lastKey).pop();
-        if(fs.get(lastKey).isEmpty()){
-            fs.remove(lastKey);
+      
+        int x=fs.get(maxFreq).pop();
+        if(fs.get(maxFreq).isEmpty()){
+            maxFreq--;
          }
         freq.put(x,freq.getOrDefault(x,0)-1);
          return x;
