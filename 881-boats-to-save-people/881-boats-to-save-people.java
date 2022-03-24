6a1a8e49beb5e8cac297049828ec340ea5036121
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-03-24 16:23:46 +0530

    Time: 22 ms (40.25%), Space: 65.4 MB (42.76%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.java
+++ 881-boats-to-save-people/881-boats-to-save-people.java
@@ -1,23 +1,39 @@
+// class Solution {
+//     public int numRescueBoats(int[] people, int limit) {
+//        Arrays.sort(people);
+//         int i=0;
+//         int j=people.length-1;
+//         int count=0; 
+        
+//         while(i<=j){
+//             int sum=people[i]+people[j];
+//              if(sum<=limit){
+//                 count++;
+                
+//                 i++;
+//                 j--;
+                 
+//             }else if(sum>limit){
+//                 count++;
+//                 j--;
+//             }
+//         }
+//         return count;
+//     }
+// }
+
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
-       Arrays.sort(people);
-        int i=0;
-        int j=people.length-1;
-        int count=0; 
-        
-        while(i<=j){
-            int sum=people[i]+people[j];
-             if(sum<=limit){
-                count++;
-                
-                i++;
-                j--;
-                 
-            }else if(sum>limit){
-                count++;
-                j--;
-            }
-        }
-        return count;
+        Arrays.sort(people);
+        int n=people.length;
+        int i,j;
+        int count=0;
+         for(i=0,j=n-1;i<=j;j--){
+           if(people[i]+people[j]<=limit){
+           i++; 
+           }
+          count++;
+     }
+     return count;
     }
 }
