93ca71c18ea6b6d0ae94e6bb91f9842851067535
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-03-26 23:38:31 +0530

    Added solution - LeetHub

--- Maximum Product Subarray - GFG/maximum-product-subarray.java
+++ Maximum Product Subarray - GFG/maximum-product-subarray.java
@@ -41,3 +41,20 @@ class Solution {
        return ans; 
     }
 }
+
+/*class Solution{
+public long maxProduct(int[] nums,int n){
+    //   int n=nums.length;
+      long result=nums[0];
+      int curr_max=nums[0];
+      int curr_min=nums[0];
+      for(int i=1;i<n;i++){
+            int curr_max_prev=curr_max;
+            curr_max=Math.max(nums[i],Math.max(curr_max*nums[i],curr_min*nums[i]));
+             curr_min=Math.min(nums[i],Math.min(curr_max_prev*nums[i],curr_min*nums[i]));
+              result=Math.max(result,curr_max);
+       }
+      
+      return result;
+     }
+}*/
