c70293af06183618924e9c56f55a1d560599ce10
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-03-27 15:58:26 +0530

    Time: 2 ms (88.13%), Space: 48.2 MB (72.54%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -32,27 +32,52 @@
 // }
 
 
+// class Solution {
+//     public int[] kWeakestRows(int[][] mat, int k) {
+       
+//      PriorityQueue<int[]> pq=new PriorityQueue<>((a,b) -> a[0]!=b[0] ? b[0]-a[0]:b[1]-a[1]);
+      
+//       int[] res=new int[k];
+//       for(int i=0;i<mat.length;i++){
+//                int count=0;
+//          for(int j=0;j<mat[0].length;j++){
+//               if(mat[i][j]==1){
+//                      count++;
+//                   }else break;
+//             }
+//            pq.offer(new int[]{count,i});
+//          }
+      
+//       while(pq.size()>k){
+//                 pq.poll();
+//            }
+
+//        while(k>0){res[--k]=pq.poll()[1];}
+//         return res;
+//     }
+// }
+
 class Solution {
     public int[] kWeakestRows(int[][] mat, int k) {
        
-     PriorityQueue<int[]> pq=new PriorityQueue<>((a,b) -> a[0]!=b[0] ? b[0]-a[0]:b[1]-a[1]);
+    int count[]=new int[mat.length];
       
       int[] res=new int[k];
       for(int i=0;i<mat.length;i++){
-               int count=0;
+               int count1=0;
          for(int j=0;j<mat[0].length;j++){
               if(mat[i][j]==1){
-                     count++;
+                     count1++;
                   }else break;
             }
-           pq.offer(new int[]{count,i});
+           count[i]=count1*1000+i;
          }
       
-      while(pq.size()>k){
-                pq.poll();
-           }
+      Arrays.sort(count);
 
-       while(k>0){res[--k]=pq.poll()[1];}
+       for(int i=0;i<k;i++){
+         res[i]=count[i]%1000;
+        }
         return res;
     }
 }
