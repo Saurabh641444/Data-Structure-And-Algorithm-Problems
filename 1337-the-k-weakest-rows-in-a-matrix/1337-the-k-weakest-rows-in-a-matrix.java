1345bd3d79074217789963217cc60786d9730178
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-03-27 15:50:13 +0530

    Time: 3 ms (60.90%), Space: 47.5 MB (76.77%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -1,32 +1,58 @@
+// class Solution {
+//     public int[] kWeakestRows(int[][] mat, int k) {
+       
+//      int rows=mat.length;
+//      int cols=mat[0].length;
+      
+//      int arr[]=new int[rows];
+//      for(int i=0;i<rows;i++){
+//       int count1=0;
+//            for(int j=0;j<cols;j++){
+//                if(mat[i][j]==1){count1++;}
+//               }
+//              arr[i]=count1;
+//          }
+        
+//      int ans[]=new int[k];
+//      for(int i=0;i<k;i++){
+//            int min=Integer.MAX_VALUE;
+//            int minIndex=0;
+//            for(int j=0;j<arr.length;j++){
+//                if(arr[j]<min){
+//                    min=arr[j];
+//                    minIndex=j;
+//                  }
+//              }
+//            ans[i]=minIndex;
+//            arr[minIndex]=Integer.MAX_VALUE;
+//         }
+//     return ans;
+        
+//     }
+// }
+
+
 class Solution {
     public int[] kWeakestRows(int[][] mat, int k) {
        
-     int rows=mat.length;
-     int cols=mat[0].length;
+     PriorityQueue<int[]> pq=new PriorityQueue<>((a,b) -> a[0]!=b[0] ? b[0]-a[0]:b[1]-a[1]);
       
-     int arr[]=new int[rows];
-     for(int i=0;i<rows;i++){
-      int count1=0;
-           for(int j=0;j<cols;j++){
-               if(mat[i][j]==1){count1++;}
-              }
-             arr[i]=count1;
+      int[] res=new int[k];
+      for(int i=0;i<mat.length;i++){
+               int count=0;
+         for(int j=0;j<mat[0].length;j++){
+              if(mat[i][j]==1){
+                     count++;
+                  }else break;
+            }
+           pq.offer(new int[]{count,i});
          }
-        
-     int ans[]=new int[k];
-     for(int i=0;i<k;i++){
-           int min=Integer.MAX_VALUE;
-           int minIndex=0;
-           for(int j=0;j<arr.length;j++){
-               if(arr[j]<min){
-                   min=arr[j];
-                   minIndex=j;
-                 }
-             }
-           ans[i]=minIndex;
-           arr[minIndex]=Integer.MAX_VALUE;
-        }
-    return ans;
-        
+      
+      while(pq.size()>k){
+                pq.poll();
+           }
+
+       while(k>0){res[--k]=pq.poll()[1];}
+        return res;
     }
 }
