b413071db9b3444b771294d666e44914382cdd22
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-03-28 12:33:28 +0530

    Time: 3 ms (5.54%), Space: 43.9 MB (45.64%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
@@ -1,3 +1,4 @@
+/* TC O(N+NlogN) SC O(!)
 class Solution {
     public boolean search(int[] nums, int target) {
         Arrays.sort(nums);
@@ -5,6 +6,29 @@ class Solution {
            if(nums[i]==target){
                return true;
            } 
+        }
+        
+        return false;
+    }
+}
+*/
+
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int l=0;
+        int r=nums.length-1;
+        int mid=0;
+        Arrays.sort(nums);
+        while(l<=r){
+            mid=l+(r-l)/2;
+            
+            if(nums[mid]==target){
+                return true;
+            }else if(nums[mid]>target){
+                r=mid-1;
+            }else if(nums[mid]<target){
+                l=mid+1;
+            }
         }
         
         return false;
