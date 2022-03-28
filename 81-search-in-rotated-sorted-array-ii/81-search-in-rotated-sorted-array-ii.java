0e84efe3f0ef41bea1c0747301c48ddef1a131d7
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-03-28 13:12:39 +0530

    Time: 0 ms (100.00%), Space: 42 MB (89.98%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
@@ -13,21 +13,55 @@ class Solution {
 }
 */
 
+// class Solution {
+//     public boolean search(int[] nums, int target) {
+//         int l=0;
+//         int r=nums.length-1;
+//         int mid=0;
+//         Arrays.sort(nums);
+//         while(l<=r){
+//             mid=l+(r-l)/2;
+            
+//             if(nums[mid]==target){
+//                 return true;
+//             }else if(nums[mid]>target){
+//                 r=mid-1;
+//             }else if(nums[mid]<target){
+//                 l=mid+1;
+//             }
+//         }
+        
+//         return false;
+//     }
+// }
+
 class Solution {
     public boolean search(int[] nums, int target) {
         int l=0;
         int r=nums.length-1;
         int mid=0;
-        Arrays.sort(nums);
+       
         while(l<=r){
             mid=l+(r-l)/2;
             
             if(nums[mid]==target){
                 return true;
-            }else if(nums[mid]>target){
-                r=mid-1;
-            }else if(nums[mid]<target){
+            }else if(nums[l]<nums[mid]){
+                if(nums[l]<=target&&nums[mid]>=target){
+                r=mid-1;}
+                else{
+                    l=mid+1;
+                }
+            }else if(nums[l]>nums[mid]){
+             if(nums[r]>=target&&nums[mid]<=target){
                 l=mid+1;
+            }
+                else{
+                    r=mid-1;
+                }
+            }
+            else{
+                l++;
             }
         }
         
