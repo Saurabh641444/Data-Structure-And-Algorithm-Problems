9fcf9e20867085b20ca2296ec5a26ea434acff0d
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-04-02 13:18:45 +0530

    Time: 2 ms (99.61%), Space: 42 MB (97.83%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,14 +1,36 @@
+// class Solution {
+//     public boolean isPalindrome(String s) {
+//          s = s.replaceAll(
+//           "[^a-zA-Z0-9]", "");
+//          s=s.toLowerCase();
+//         // System.out.println(s);
+//         int lo=0;
+//         int hi=s.length()-1;
+//         while(lo<=hi){
+            
+//             if(s.charAt(lo)!=s.charAt(hi)){
+//                 return false;
+//             }
+            
+//             lo++;
+//             hi--;
+//         }
+        
+//         return true;
+//     }
+// }
+
 class Solution {
     public boolean isPalindrome(String s) {
-         s = s.replaceAll(
-          "[^a-zA-Z0-9]", "");
-         s=s.toLowerCase();
-        // System.out.println(s);
+        int n=s.length();
         int lo=0;
         int hi=s.length()-1;
         while(lo<=hi){
             
-            if(s.charAt(lo)!=s.charAt(hi)){
+            while(lo<n && !Character.isLetterOrDigit(s.charAt(lo))){lo++;}
+            while(hi>=0 && !Character.isLetterOrDigit(s.charAt(hi))){hi--;}      
+            if(lo>hi){break;}
+            if(Character.toLowerCase(s.charAt(lo))!=Character.toLowerCase((s.charAt(hi)))){
                 return false;
             }
             
