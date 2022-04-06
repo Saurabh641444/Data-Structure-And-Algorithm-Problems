000cee3cb2f57f947c4cfaf2e229f9749bf318f6
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-04-06 12:56:16 +0530

    Time: 4 ms (80.65%), Space: 44.9 MB (25.40%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.java
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.java
@@ -1,31 +1,52 @@
 class Solution {
     public int threeSumMulti(int[] arr, int target) {
         // Arrays.sort(arr);
-        int i=0;
-        int n=arr.length;
-        long count=0;
-        long mod=1000000007;
-        while(i<n){
-            int newTarget=target-arr[i];
-            int l=i+1;
-            int r=n;
-           HashMap<Integer,Integer> map=new HashMap<>();
+//         int i=0;
+//         int n=arr.length;
+//         long count=0;
+//         long mod=1000000007;
+//         while(i<n){
+//             int newTarget=target-arr[i];
+//             int l=i+1;
+//             int r=n;
+//            HashMap<Integer,Integer> map=new HashMap<>();
             
-            for(int j=l;j<r;j++){
-                if(map.get(newTarget-arr[j])!=null){
-                    count+=map.get(newTarget-arr[j]);
-                }
+//             for(int j=l;j<r;j++){
+//                 if(map.get(newTarget-arr[j])!=null){
+//                     count+=map.get(newTarget-arr[j]);
+//                 }
                 
-                if(map.get(arr[j])!=null){
-                    map.put(arr[j],map.get(arr[j])+1);
-                }else{
-                    map.put(arr[j],1);
-                }
-            }
+//                 if(map.get(arr[j])!=null){
+//                     map.put(arr[j],map.get(arr[j])+1);
+//                 }else{
+//                     map.put(arr[j],1);
+//                 }
+//             }
             
-            i++;
-        }
+//             i++;
+//         }
         
-        return (int)(count%mod);
+//         return (int)(count%mod);
+        
+        long ans=0;
+int MOD=1_000_000_007;
+long[] c=new long[101];
+for(int i:arr) c[i]++;
+
+for(int i=0;i<101;i++){
+for(int j=0;j<101;j++){
+   int k=target-i-j;
+   if(k<0 || k>100) continue;
+   if(i==j&&j==k) {
+       ans+=(c[i]*(c[i]-1)*(c[i]-2)/6);
+      }else if(i==j&&j!=k){
+       ans+=(c[i]*(c[i]-1)/2)*(c[k]);
+       }else if(i<j&&j<k){
+          ans+=(c[i]*c[j]*c[k]);
+          }
+ }
+}
+
+return (int)(ans%MOD);
     }
 }
