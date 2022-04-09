fb820839fab556b7a66958b01b7939b856957582
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-04-09 13:12:07 +0530

    Time: 12 ms (81.89%), Space: 51.2 MB (17.14%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer,Integer> map=new HashMap<>();
         
@@ -28,5 +28,29 @@ class Solution {
      }
         
      return null;   
+    }
+}*/
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+    List<Integer>[] bucket=new List[nums.length+1];
+    HashMap<Integer,Integer> hm=new HashMap<>();
+
+   for(int i=0;i<nums.length;i++){ hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);} 
+
+   for(int key:hm.keySet()){
+      int frequency =hm.get(key);
+      if(bucket[frequency]==null){ bucket[frequency]=new ArrayList<>();}
+      bucket[frequency].add(key);
+    }
+    int ans[]=new int[k];
+   int count=0;
+  for(int i=bucket.length-1;i>=0;i--){
+       if(bucket[i]!=null){
+            for(int j=0;j<bucket[i].size();j++){ ans[count++]=bucket[i].get(j);}
+         }
+      if(count==k) break;
+      }
+ return ans;
     }
 }
