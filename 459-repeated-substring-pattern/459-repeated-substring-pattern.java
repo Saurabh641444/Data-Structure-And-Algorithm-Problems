adbbc137a824bac674c0507b5994dc8fb5a2723c
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-04-11 13:15:05 +0530

    Time: 2211 ms (5.03%), Space: 282.1 MB (9.31%) - LeetHub

---
+++ 459-repeated-substring-pattern/459-repeated-substring-pattern.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+    if(s.length()==0 || s.length()==1) return false;
+    
+    return helper(s,""+s.charAt(0),1);
+    }
+
+     public boolean helper(String s,String repeat,int plus){
+          if(s.equals(repeat)) return false;
+           
+          int i=0;
+          while(i<s.length()){
+                   if(i+repeat.length()<=s.length() &&
+repeat.equals(s.substring(i,i+repeat.length()))){
+             i=i+repeat.length();
+        }else{
+                 return helper(s,s.substring(0,plus+1),plus+1); 
+               }
+             }
+       return true;
+      }
+}
