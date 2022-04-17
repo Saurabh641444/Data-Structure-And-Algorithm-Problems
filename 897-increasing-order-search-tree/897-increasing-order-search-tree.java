a5a7f205d4d49e12f9e80d914b3c29613b943ef7
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-04-17 15:18:04 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (95.34%) - LeetHub

--- 897-increasing-order-search-tree/897-increasing-order-search-tree.java
+++ 897-increasing-order-search-tree/897-increasing-order-search-tree.java
@@ -13,7 +13,7 @@
  *     }
  * }
  */
-class Solution {
+/*class Solution {
     public TreeNode increasingBST(TreeNode root) {
        
      ArrayList<Integer> al=new ArrayList<>();
@@ -33,5 +33,31 @@ class Solution {
       inorder(root.left,al);
       al.add(root.val);
       inorder(root.right,al);
+      }
+}*/
+
+class Solution {
+  
+  private TreeNode head=null,parent=null;  
+  public TreeNode increasingBST(TreeNode root) {
+       
+     
+     solve(root);
+     return head;      
+    }
+
+    
+   
+    private void solve(TreeNode root){
+      if(root==null) return;
+      solve(root.left);
+      if(head==null) head=root;
+      if(parent==null) parent=root;
+      else{
+           parent.right=root;
+           parent=root;
+           root.left=null;
+         }
+       solve(root.right);
       }
 }
