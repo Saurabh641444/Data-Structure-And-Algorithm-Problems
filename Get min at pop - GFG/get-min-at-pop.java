14ef3535dc5779973b421e9bc72dc33fd541fc55
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-04-22 22:58:20 +0530

    Added solution - LeetHub

---
+++ Get min at pop - GFG/get-min-at-pop.java
@@ -0,0 +1,76 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class getMin
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        //Taking all the testcases
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            //Taking total number of elements
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            
+            //adding all the elements to the array
+            for(int i = 0; i < n; i++)
+              arr[i] = sc.nextInt();
+             
+            //calling _push method of class GetMin 
+            //and storing the result in a new Stack
+            Stack<Integer>mys = new GetMin()._push(arr,n);
+            
+            //calling _getMinAtPop method of class GetMin
+            new GetMin()._getMinAtPop(mys);
+            System.out.println();
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class GetMin
+{
+    //Function to push all the elements into the stack.
+    public static Stack<Integer> _push(int arr[],int n)
+    {
+        // your code here
+        Stack<Integer> stack=new Stack<Integer> ();
+        for(int i=0;i<n;i++){
+            stack.push(arr[i]);
+        }
+        
+        return stack;
+    }
+    
+    //Function to print minimum value in stack each time while popping.
+    static void _getMinAtPop(Stack<Integer>s)
+    {
+        // your code here
+        
+       
+      
+        while(!s.isEmpty()){
+             int min=Integer.MAX_VALUE;
+    
+           for(int x:s){
+            if(x<min){
+                min=x;
+            }}
+             s.pop();
+             System.out.print(min+" ");
+        }
+        
+        // System.out.print(min+" ");
+    }
+}
