1b9c8891b19e470c75e4a87bf04eb7fb6e601ee1
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-05-01 10:20:57 +0530

    Time: 50 ms (100.00%), Space: 61.2 MB (100.00%) - LeetHub

---
+++ 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minimumCardPickup(int[] cards) {
+        HashMap<Integer,Integer> hm=new HashMap<>();
+int min=Integer.MAX_VALUE;
+for(int i=0;i<cards.length;i++){
+    if(!hm.containsKey(cards[i])){
+      hm.put(cards[i],i);
+    }else{
+          min=Math.min(min,(i-hm.get(cards[i]))+1);
+          hm.put(cards[i],i);
+      }
+   
+}
+
+return min==Integer.MAX_VALUE ? -1:min;
+    }
+}
