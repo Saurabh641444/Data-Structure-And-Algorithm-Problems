f0648e9b260f2f749139597cb74d5c3f509a282a
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-05-09 11:56:35 +0530

    Create BenjaminBulb.java

---
+++ BenjaminBulb.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+class HelloWorld {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int n1=sc.nextInt();
+   
+    
+     for(int i=1;i*i<=n1;i++){
+          System.out.println(i*i);
+     }
+    
+    
+    }
+}
