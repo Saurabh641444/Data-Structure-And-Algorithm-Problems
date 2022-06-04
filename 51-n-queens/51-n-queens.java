fd28e2ac775071c6865186871876d71d735144aa
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-06-04 16:37:52 +0530

    Time: 1 ms (100.00%), Space: 42.3 MB (95.51%) - LeetHub

--- 51-n-queens/51-n-queens.java
+++ 51-n-queens/51-n-queens.java
@@ -1,3 +1,4 @@
+/* Recursive Solution
 class Solution {
     public List<List<String>> solveNQueens(int n) {
       List<List<String>> res=new ArrayList<List<String>>();
@@ -57,6 +58,60 @@ class Solution {
 
      return true;
     }
+
+    private List<String> construct(char[][] board){
+       List<String> res=new LinkedList<String> ();
+       for(int i=0;i<board.length;i++){
+         String s=new String(board[i]);
+         res.add(s);
+         }
+ return res;
+     }
+}*/
+
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+      List<List<String>> res=new ArrayList<List<String>>();
+        
+        char[][] board=new char[n][n];
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                board[i][j]='.';
+            }
+        }
+        
+        int leftRow[]=new int[n];
+        int upperDiagonal[]=new int[2*n-1];
+        int lowerDiagonal[]=new int[2*n-1];
+        dfs(0,board,res,leftRow,upperDiagonal,lowerDiagonal);
+        
+        return res;
+        
+    }
+    
+    private void dfs(int col,char[][] board,List<List<String>> res, int leftRow[], int upperDiagonal[],int lowerDiagonal[]){
+      
+         if(col==board.length){
+                   res.add(construct(board));
+               return;}        
+
+        for(int row=0;row<board.length;row++){
+                  if(leftRow[row]==0 && upperDiagonal[board.length-1+col-row]==0 && lowerDiagonal[row+col]==0){
+                      board[row][col]='Q';
+                      leftRow[row]=1;
+                      upperDiagonal[board.length-1+col-row]=1;
+                      lowerDiagonal[col+row]=1;
+                      dfs(col+1,board,res,leftRow,upperDiagonal,lowerDiagonal);
+                       board[row][col]='.';
+                      leftRow[row]=0;
+                      upperDiagonal[board.length-1+col-row]=0;
+                      lowerDiagonal[col+row]=0;
+                     }
+               }
+    }
+    
+    
 
     private List<String> construct(char[][] board){
        List<String> res=new LinkedList<String> ();
