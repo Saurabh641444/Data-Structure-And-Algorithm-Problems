c7fe48eefe43d1fd9e382dd76190a20e75aaa239
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-06-07 11:47:46 +0530

    Time: 1 ms (91.89%), Space: 43.1 MB (17.55%) - LeetHub

---
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+         List<List<Integer>> res=new ArrayList<List<Integer>>();
+          if(root==null) return res;
+          Stack<TreeNode> s1=new Stack<>();
+          Stack<TreeNode> s2=new Stack<>();
+          s1.push(root);
+        
+        while(!s1.isEmpty()||!s2.isEmpty()){
+            List<Integer> tmp=new ArrayList<>();
+            while(!s1.isEmpty()){
+                TreeNode c=s1.pop();
+                tmp.add(c.val);
+                if(c.left!=null){ s2.push(c.left);}
+                if(c.right!=null){ s2.push(c.right);}
+            }
+            res.add(tmp);
+            tmp=new ArrayList<>();
+            while(!s2.isEmpty()){
+                TreeNode c=s2.pop();
+                tmp.add(c.val);
+                
+                if(c.right!=null){ s1.push(c.right);}
+                if(c.left!=null){ s1.push(c.left);}
+            }
+            
+            if(!tmp.isEmpty()) res.add(tmp);
+        }
+        
+        return res;
+    }
+    
+   
+}
