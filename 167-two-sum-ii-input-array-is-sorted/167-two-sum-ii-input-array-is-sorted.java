513d3ae0d1f5400a7ec1a211ee643d017a81f44e
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-06-09 17:33:15 +0530

    Time: 7 ms (11.27%), Space: 50.3 MB (23.58%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
@@ -1,17 +1,38 @@
+// class Solution {
+//     public int[] twoSum(int[] numbers, int target) {
+//         int n=numbers.length;
+//         int start=0, end=n-1;
+
+//         while(start<end){
+//               if(numbers[start]+numbers[end]>target){
+//         end--;
+// }else if(numbers[start]+numbers[end]<target){
+//         start++;
+//          }else{
+//             return new int[]{start+1,end+1};
+//               }
+//            }
+//        return new int[]{};//space O(1), time O(N);
+//     }
+// }
+
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int n=numbers.length;
-        int start=0, end=n-1;
+       int end=numbers.length-1;
+      for(int i=0;i<numbers.length;i++){
 
-        while(start<end){
-              if(numbers[start]+numbers[end]>target){
-        end--;
-}else if(numbers[start]+numbers[end]<target){
-        start++;
-         }else{
-            return new int[]{start+1,end+1};
-              }
-           }
-       return new int[]{};//space O(1), time O(N);
-    }
-}
+        int start=i+1;
+        while(start<=end){
+           int mid=start+(end-start)/2;
+             if(numbers[i]+numbers[mid]>target){
+            end=mid-1;
+          }else if(numbers[i]+numbers[mid]<target){
+             start=mid+1;
+            }else{
+                return new int[]{i+1,mid+1};
+               }
+            
+        }
+      }
+        return new int[2];
+    }}
