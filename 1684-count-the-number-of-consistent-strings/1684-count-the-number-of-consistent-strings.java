6423772356e0a687ca7afe0c2b5131d7e12c3f1c
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-06-10 12:45:20 +0530

    Time: 7 ms (94.68%), Space: 53.9 MB (67.85%) - LeetHub

--- 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.java
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int countConsistentStrings(String allowed, String[] words) {
         HashSet<Character> set=new HashSet<>();
         for(int i=0;i<allowed.length();i++){
@@ -15,6 +15,31 @@ class Solution {
                 
                 if(j==str.length()-1) { count++;}
             }
+        }
+        
+        return count;
+        
+    }
+}*/
+
+// Bit Manipulation Solution
+
+class Solution {
+    public int countConsistentStrings(String allowed, String[] words) {
+         int count=words.length;
+           
+        int bitset=0;
+        
+        for(int i=0;i<allowed.length();i++){
+            bitset=bitset |(1<<(allowed.charAt(i)-'a'));
+        }
+        
+        for(int i=0;i<words.length;i++){
+            String str=words[i];
+            for(int j=0;j<str.length();j++){
+                if((bitset & (1<<str.charAt(j)-'a'))==0) {count--; break;}
+            }
+            
         }
         
         return count;
