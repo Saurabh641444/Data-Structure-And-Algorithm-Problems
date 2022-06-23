dc01faf4965f35f9b3b88e3b92c7a0d08a750655
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-06-23 18:42:41 +0530

    Added solution - LeetHub

---
+++ 11. Swap all odd and even bits - GFG/11.-swap-all-odd-and-even-bits.java
@@ -0,0 +1,42 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+ // } Driver Code Ends
+//User function Template for Java
+
+class Solution
+{
+    //Function to swap odd and even bits.
+    public static int swapBits(int n) 
+    {
+	    // Your code
+	    int x=(n & 0xAAAAAAAA)>>1;
+	    int y=(n & 0x55555555)<<1;
+	    
+	    return x|y;
+	}
+    
+}
+
+// { Driver Code Starts.
+
+class BitWise{
+
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();//testcases
+		while(t-->0){
+		    int n = sc.nextInt();//taking n
+		    
+		    Solution obj = new Solution();
+		    
+		    //calling swapBits() method of class
+		    //Swap
+		    System.out.println(obj.swapBits(n));
+		}
+	}
+}  // } Driver Code Ends
