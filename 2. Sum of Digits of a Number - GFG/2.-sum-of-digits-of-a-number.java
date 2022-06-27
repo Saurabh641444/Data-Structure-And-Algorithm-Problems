b74be210b7fb881318b0b5eeb29ac4cb57822256
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-06-27 18:57:08 +0530

    Added solution - LeetHub

---
+++ 2. Sum of Digits of a Number - GFG/2.-sum-of-digits-of-a-number.java
@@ -0,0 +1,43 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Driverclass
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();//taking testcases
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();  // taking number "n" as input
+            System.out.println(new Solution().sumOfDigits(n)); // computing sum of digits of n
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    // complete the function
+    public static int sumOfDigits(int n)
+    {
+        // add your code here
+        int res=0;
+        
+        while(n>0){
+            res=res+n%10;
+            n=n/10;
+        }
+        
+        return res;
+    }
+}
+
