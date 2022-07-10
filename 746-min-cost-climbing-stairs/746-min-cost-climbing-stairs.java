934b05249feb9143d247f6c4bcf3743782503894
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-07-10 19:56:15 +0530

    Time: 2 ms (32.47%), Space: 43.6 MB (56.98%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int minCostClimbingStairs(int[] cost) {
        int n=cost.length;
         
@@ -12,4 +12,31 @@ class Solution {
     }
     
    
+}*/
+
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+       int n=cost.length;
+        
+        int arr[]=new int[n];
+        
+        arr[0]=cost[0];
+        arr[1]=cost[1];
+        
+        return new Recursion().solve(cost,2,n,arr);
+       
+    }
+    
+   
+}
+
+class Recursion{
+    
+    public int solve(int cost[],int i,int n,int []arr){
+        if(i==n){
+            return Math.min(arr[n-1],arr[n-2]); 
+        }
+         arr[i]=cost[i]+Math.min(arr[i-1],arr[i-2]);
+         return solve(cost,i+1,n,arr);
+    } 
 }
