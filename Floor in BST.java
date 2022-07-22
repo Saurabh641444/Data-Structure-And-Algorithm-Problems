9d7bbce807abc2033ce9cc5fada8876481d7da3d
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-07-22 17:13:07 +0530

    Create Floor in BST.java

---
+++ Floor in BST.java
@@ -0,0 +1,43 @@
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    class TreeNode<T>
+    {
+       public:
+        T data;
+        TreeNode<T> left;
+        TreeNode<T> right;
+
+        TreeNode(T data)
+        {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    };
+
+************************************************************/
+
+public class Solution {
+
+    public static int floorInBST(TreeNode<Integer> root, int X) {
+        //    Write your code here.
+        int floor=-1;
+        while(root!=null){
+            if(root.data==X){
+                floor=root.data;
+                return floor;
+            }
+            
+            if(X>root.data){
+                floor=root.data;
+                root=root.right;
+            }else{
+                root=root.left;
+            }
+        }
+        
+        return floor;
+    }
+}
