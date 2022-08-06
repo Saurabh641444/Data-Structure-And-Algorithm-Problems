62ff720ff968ae464a4046882d0eac0d830e43e4
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-08-06 14:03:46 +0530

    Create Kth_MEX.java

---
+++ Kth_MEX.java
@@ -0,0 +1,16 @@
+class solution{
+
+  static int kthMex(int n,int k,int arr[]){
+
+    HashSet<Integer> set=new HashSet<>();
+
+   for(int i=0;i<n;i++){ se.add(arr[i]);}
+
+    for(int i=0;;i++){
+       if(set.contains(i)==false) k--;
+      if(k==0){ return i;}
+         }
+   }
+
+
+}
