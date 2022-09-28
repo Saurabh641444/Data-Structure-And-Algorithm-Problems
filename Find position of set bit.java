cc9720e491c5ca29123dcdb800d88e1fe972458c
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-09-28 19:29:15 +0530

    Create Find position of set bit.java

---
+++ Find position of set bit.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.findPosition(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int findPosition(int N) {
+        // code here
+        if(N==0) return -1;
+        if(N==1) return 1;
+        if(N%2!=0) return -1;
+        
+        int count=0;
+        int pos=0;
+        while(N>0){
+            int rem=N%2;
+            N=N/2;
+            if(rem==1) count++;
+            pos++;
+        }
+        
+        if(count>1) return -1;
+        
+        return pos;
+    }
+};
