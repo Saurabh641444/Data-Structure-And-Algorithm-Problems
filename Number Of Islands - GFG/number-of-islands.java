10affd8428e9dddfc02a5f2fe6ea667813c52ddf
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-10-10 22:53:08 +0530

    Added solution - LeetHub

---
+++ Number Of Islands - GFG/number-of-islands.java
@@ -0,0 +1,207 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+/*
+class Solution {
+    
+    public List<Integer> numOfIslands(int rows, int cols, int[][] operators) {
+        //Your code here
+        List<Integer> ans=new ArrayList<>();
+        
+        int grid[][]=new int[rows][cols];
+        
+        for(int i=0;i<operators.length;i++){
+            int r=operators[i][0];
+            int c=operators[i][1];
+            
+            grid[r][c]=1;
+            
+            ans.add(helper(grid,rows,cols));
+        }
+        
+        return ans;
+    }
+    
+    public int helper(int grid[][], int rows, int cols){
+        int count=0;
+        boolean vis[][]=new boolean[rows][cols];
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(grid[i][j]==1 && vis[i][j]==false){
+                    dfs(grid,i,j,vis,rows,cols);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    
+    public void dfs(int [][] grid,int i,int j,boolean[][] vis,int m,int n){
+        
+        vis[i][j]=true;
+        
+        int x[]={0,1, 0,-1};
+        int y[]={1,0,-1, 0};
+        
+        for(int k=0;k<4;k++){
+            
+            int new_x=i+x[k];
+            int new_y=j+y[k];
+            
+            if(new_x>=0 && new_y>=0 && new_x<m && new_y<n && vis[new_x][new_y]==false && grid[new_x][new_y]==1){
+                dfs(grid,new_x,new_y,vis,m,n);
+            }
+        }
+        
+    }
+    
+}*/
+
+class Solution {
+    
+    public List<Integer> numOfIslands(int rows, int cols, int[][] operators) {
+        //Your code here
+        List<Integer> ans=new ArrayList<>();
+        
+        int grid[][]=new int[rows][cols];
+      
+       int V=rows*cols;
+       
+       UnionFind obj=new UnionFind(V,rows,cols);
+       
+       int x[]={0,1, 0,-1};
+       int y[]={1,0,-1, 0};
+       
+        for(int k=0;k<operators.length;k++){
+            int r=operators[k][0];
+            int c=operators[k][1];
+            
+            grid[r][c]=1;
+            
+     
+                for(int d=0;d<4;d++){
+                    int new_x=r+x[d];
+                    int new_y=c+y[d];
+                    
+                    if(new_x>=0 && new_y>=0 && new_x<rows && new_y<cols && grid[new_x][new_y]==1){
+                        obj.union(r*cols+c,new_x*cols+new_y);
+                        // System.out.println("Euu");
+                    }
+                }
+            
+            
+            
+            ans.add(obj.getCount(grid));
+        }
+        
+        return ans;
+    }
+    
+   
+
+}
+
+class UnionFind{
+    
+    private int count;
+    private int[] root;
+    private int[] rank;
+    private int m;
+    private int n;
+    
+    UnionFind(int V,int m,int n){
+        
+        root=new int[V];
+        rank=new int[V];
+        this.m=m;
+        this.n=n;
+        
+        for(int i=0;i<V;i++){
+            root[i]=i;
+            rank[i]=1;
+        }
+    }
+    
+    int find(int x){
+        if(x==root[x]){
+            return x;
+        }
+        
+        return x=find(root[x]);
+    }
+    
+    void union(int x,int y){
+        int rootX=find(x);
+        int rootY=find(y);
+        //  root[rootY]=rootX;
+      if(rootX!=rootY){
+            
+            if(rank[rootX]>rank[rootY]){
+                root[rootY]=rootX;
+            }else if(rank[rootX]<rank[rootY]){
+                root[rootX]=rootY;
+            }else{
+                root[rootY]=rootX;
+                rank[rootX]++;
+            }
+
+           
+        }
+        
+    }
+    
+    int getCount(int [][] grid){
+     count=0;
+     
+     
+     for(int i=0;i<m;i++){
+         for(int j=0;j<n;j++){
+             if(grid[i][j]==1 && root[i*n+j]==i*n+j){
+                 count++;
+                //  System.out.println("Euu");
+             }
+         }
+     }
+     
+    //  System.out.println(count);
+     return count;
+    }
+    
+}
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int  k= sc.nextInt();
+            int[][] a = new int[k][2];
+            for(int i=0;i<k;i++){
+            
+                a[i][0] = sc.nextInt();
+                a[i][1] = sc.nextInt();
+            }
+            
+            Solution obj = new Solution();
+            List<Integer> ans = obj.numOfIslands(n,m,a);
+           
+            for(int i:ans){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
