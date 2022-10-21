94e43f0147cfe1f5051c4a854b343de55a2713ae
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-10-21 12:51:45 +0530

    Added solution - LeetHub

--- Hamiltonian Path - GFG/hamiltonian-path.java
+++ Hamiltonian Path - GFG/hamiltonian-path.java
@@ -40,42 +40,46 @@ class Solution
     boolean check(int N, int M, ArrayList<ArrayList<Integer>> Edges) 
     { 
         // code here
-        n = N;
-        Map<Integer,ArrayList<Integer>> map = new HashMap<>();
-        for(ArrayList<Integer> edges : Edges){
-            int u = edges.get(0);
-            int v = edges.get(1);
-            map.putIfAbsent(u,new ArrayList<>());
-            map.putIfAbsent(v,new ArrayList<>());
-            map.get(u).add(v);
-            map.get(v).add(u);
-        }
-        int cnt = 1;
-        // Set<Integer> set = new HashSet<>();
-        boolean[] visited = new boolean[N+1];
-        for(int i = 1; i<=N; i++){
-            if(dfs(i,map,visited,cnt)){
-                return true;
-            }
-        }
-        return false;
-    }
-    
-    
-    boolean dfs(int node,Map<Integer,ArrayList<Integer>> map,boolean[] visited,int cnt){
-        if(cnt == (visited.length - 1)){
-            return true;
+        Map<Integer,ArrayList<Integer>> hm=new HashMap<>();
+        n=N;
+        
+        for(ArrayList<Integer> edge:Edges){
+           int src=edge.get(0);
+           int des=edge.get(1);
+           
+           hm.putIfAbsent(src,new ArrayList<Integer>());
+           hm.putIfAbsent(des,new ArrayList<Integer>());
+           
+           hm.get(src).add(des);
+           hm.get(des).add(src);
         }
         
-        visited[node] = true;
-        cnt += 1;
-        for(Integer neighbour : map.get(node)){
-            if(!visited[neighbour] && dfs(neighbour,map,visited,cnt)){
+       Set<Integer> set=new HashSet<Integer> ();
+        
+        for(int i=1;i<=N;i++){
+            if(dfs(hm,set,i)){
                 return true;
             }
         }
-        cnt--;
-        visited[node] = false;// backtracking
+        
         return false;
     }
+    
+    boolean dfs(Map<Integer,ArrayList<Integer>> hm,Set<Integer> set,int src){
+        
+        set.add(src);
+        
+        if(set.size()==n) return true;
+        
+        for(int des:hm.get(src)){
+            if(set.contains(des)==false && dfs(hm,set,des)){
+                return true;
+            }
+        }
+        
+        set.remove(src);
+        return false;
+    }
+    
+   
 }
