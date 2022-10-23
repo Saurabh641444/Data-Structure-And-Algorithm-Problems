4d948681f0f4be03b3651030a0e59761afa11a38
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-10-23 22:15:17 +0530

    Added solution - LeetHub

---
+++ Stepping Numbers - GFG/stepping-numbers.java
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            String input_line[] = read.readLine().trim().split("\\s+");
+            int  N = Integer.parseInt(input_line[0]);
+            int  M = Integer.parseInt(input_line[1]);
+            
+            Solution ob = new Solution();
+            int ans = ob.steppingNumbers(N, M);
+            System.out.println(ans);
+        }
+    }
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    int ans;
+    int steppingNumbers(int n, int m){
+        // code here
+        /*
+        10 , 11, 12, 13, 14, 15
+        stepping number 10,12
+        
+        */
+        ans=0;
+        
+        if(n==0) ans++;
+        
+        for(int i=1;i<=9;i++){
+            dfs(i,n,m);
+        }
+        
+        return ans;
+    }
+    
+    void dfs(int curr,int n,int m){
+        
+        if(curr>m) return;
+    
+        if(curr<=m && curr>=n) ans++;    
+        int lastDigit=curr%10;
+        
+        if(lastDigit==9){
+            dfs(curr*10+8,n,m);
+        }else if(lastDigit==0){
+            dfs(curr*10+1,n,m);
+        }else{
+            dfs(curr*10+(lastDigit+1),n,m);
+            dfs(curr*10+(lastDigit-1),n,m);
+        }
+    }
+}
