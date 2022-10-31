6deea7643508db7005344f41e374f7c22c7435e8
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-10-31 14:46:18 +0530

    Time: 1 ms (93.83%), Space: 46.2 MB (16.33%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.java
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.java
@@ -1,36 +1,38 @@
 class Solution {
     public boolean isToeplitzMatrix(int[][] matrix) {
-        ArrayList<Integer> arr=new ArrayList<>();
+        // ArrayList<Integer> arr=new ArrayList<>();
         int m=matrix.length;
         int n=matrix[0].length;
         
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 
-                if(i==m-1 && j==0){
-                    arr.add(-1);
-                    continue;
-                }
-                arr.add(matrix[i][j]);
+                // if(i==m-1 && j==0){
+                //     arr.add(-1);
+                //     continue;
+                // }
+                // arr.add(matrix[i][j]);
+                
+                if(i>0 && j>0 && matrix[i-1][j-1]!=matrix[i][j]) return false;
                 
                 
             }
         }
         
-        System.out.println(arr.size());
+//         System.out.println(arr.size());
         
-        for(int i=0;i<arr.size();i++){
+//         for(int i=0;i<arr.size();i++){
            
-            if(i%(n) ==0 || arr.get(i)==-1){ continue;
-                      }
-            else if(i-n-1>=0&&arr.get(i-n-1)!=arr.get(i)){
-                 System.out.println("i= "+ i+" ->"+arr.get(i));
-                System.out.println("i-n-1= "+ (i-n-1)+" ->"+arr.get(i-n-1));
-                return false;
-            }
+//             if(i%(n) ==0 || arr.get(i)==-1){ continue;
+//                       }
+//             else if(i-n-1>=0&&arr.get(i-n-1)!=arr.get(i)){
+//                  System.out.println("i= "+ i+" ->"+arr.get(i));
+//                 System.out.println("i-n-1= "+ (i-n-1)+" ->"+arr.get(i-n-1));
+//                 return false;
+//             }
             
             
-        }
+//         }
         return true;
     }
 }
