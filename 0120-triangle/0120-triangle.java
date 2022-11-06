aac385df09e9c1e004fe565bf7427aab9ed5fff4
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-11-06 17:57:27 +0530

    Time: 12 ms (34.40%), Space: 43.4 MB (79.42%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,10 +1,27 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int dp[][]=new int[triangle.size()+1][triangle.size()+1];
-        for(int [] a:dp){
-            Arrays.fill(a,-1);
+        int n=triangle.size();
+        
+        for(int i=0;i<n;i++){
+            dp[n-1][i]=triangle.get(n-1).get(i);
         }
-        return dfs(triangle,0,0,Integer.MAX_VALUE,dp);
+        
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+             
+            int first=dp[i+1][j];
+            int second=dp[i+1][j+1];
+           dp[i][j]=triangle.get(i).get(j)+Math.min(first,second);
+              
+            }
+        }
+        
+        return dp[0][0];
+        // for(int [] a:dp){
+        //     Arrays.fill(a,-1);
+        // }
+        // return dfs(triangle,0,0,Integer.MAX_VALUE,dp);
     }
     
     private int dfs(List<List<Integer>> triangle,int j,int start,int min,int dp[][]){
