3b030f992a9c777c1a8b426f56d7592ba9fdca55
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-11-08 14:02:43 +0530

    Update SubsetSums.GFG.java

--- SubsetSums.GFG.java
+++ SubsetSums.GFG.java
@@ -1,5 +1,4 @@
 //{ Driver Code Starts
-//Initial Template for Java
 import java.io.*;
 import java.util.*;
 
@@ -7,50 +6,131 @@ class GFG
 {
     public static void main(String args[])throws IOException
     {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
         while(t-- > 0)
         {
-            int N=sc.nextInt();
-			ArrayList<Integer> arr = new ArrayList<>();
-			for(int i = 0; i < N ; i++){
-			    arr.add(sc.nextInt());
-			}
+            int N = Integer.parseInt(read.readLine());
+            String input_line[] = read.readLine().trim().split("\\s+");
+            int arr[]= new int[N];
+            for(int i = 0; i < N; i++)
+                arr[i] = Integer.parseInt(input_line[i]);
+            int sum = Integer.parseInt(read.readLine());
+
             Solution ob = new Solution();
-         
-            ArrayList<Integer> ans = ob.subsetSums(arr,N);
-            Collections.sort(ans);
-            for(int sum : ans){
-                System.out.print(sum+" ");
-            }
-            System.out.println();
-        }  
+            if(ob.isSubsetSum(N, arr, sum))
+            System.out.println(1);
+            else
+            System.out.println(0);
+
+            
+        }
     }
 }
 
 // } Driver Code Ends
 
 
-//User function Template for Java//User function Template for Java
+//User function Template for Java
+
 class Solution{
-    ArrayList<Integer> subsetSums(ArrayList<Integer> arr, int N){
+
+
+    static Boolean isSubsetSum(int N, int arr[], int sum){
         // code here
-        ArrayList<Integer> ans=new ArrayList<>();
-        Collections.sort(arr);
-        findAllsubsetSums(arr,0,N,ans,0);
         
-        return ans;//tc sc 2^N
+        boolean[] prev=new boolean[sum+1];
+        
+        prev[0]=true;
+        if(arr[0]<=sum)
+            prev[arr[0]]=true;
+        
+        for(int ind=1;ind<N;ind++){
+             boolean[] temp=new boolean[sum+1];
+             temp[0]=true;
+            for(int target=1;target<=sum;target++){
+                
+              
+                boolean take=false;
+                
+              
+              boolean   notTake=prev[target];
+                
+                
+                if(target>=arr[ind] ){
+                   take=prev[target-arr[ind]];
+                  }
+        
+                 temp[target]= notTake || take; 
+                
+            }
+            prev=temp.clone();
+        }
+        
+        return prev[sum];
+       
+       /* boolean[][] dp=new boolean[N+1][sum+1];
+        
+        for(int i=0;i<N;i++){
+            dp[i][0]=true;
+        }
+        if(arr[0]<=sum)
+            dp[0][arr[0]]=true;
+        
+        for(int ind=1;ind<N;ind++){
+            for(int target=1;target<=sum;target++){
+                
+              
+                boolean take=false;
+                
+              
+              boolean   notTake=dp[ind-1][target];
+                
+                
+                if(target>=arr[ind] ){
+                   take=dp[ind-1][target-arr[ind]];
+                  }
+        
+                 dp[ind][target]= notTake || take; 
+                
+            }
+        }
+        
+        return dp[N-1][sum];*/
+        // int dp[][]=new int[N+1][sum+1];
+        
+        // for(int a[]:dp){
+        //     Arrays.fill(a,-1);
+        // }
+        
+        // return isSubsetSumHelper(arr,N-1,sum,dp);
     }
     
-    private void findAllsubsetSums(ArrayList<Integer> arr,int ind,int N,ArrayList<Integer> ans,int sum){
+    static boolean isSubsetSumHelper(int arr[],int i, int target,int dp[][]){
         
-        if(ind==N){
-            ans.add(sum);
-            return;
+        if(i==0) return arr[i]==target;
+        
+        if(0==target){
+            return true;
         }
-         findAllsubsetSums(arr,ind+1,N,ans,sum+arr.get(ind));
-        //  sum=sum-arr.get(ind);
-        findAllsubsetSums(arr,ind+1,N,ans,sum);
+        
+        if(dp[i][target]!=-1) return dp[i][target]==1?true:false;
+        
+        boolean notTake=isSubsetSumHelper(arr,i-1,target,dp);
+        boolean take=false;
+        
+        if(target>=arr[i]){
+            take=isSubsetSumHelper(arr,i-1,target-arr[i],dp);
+        }
+        
+       boolean ans= notTake || take;
        
+       if(ans==true){
+           dp[i][target]=1;
+       }else{
+        dp[i][target]=0;
+       }
+       return ans;
     }
+    
 }
