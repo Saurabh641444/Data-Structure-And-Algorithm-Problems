da276951c76b3f158449132f7675675b21be0073
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-12-03 19:05:07 +0530

    Time: 2296 ms (9.57%), Space: 431.9 MB (11.87%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -0,0 +1,59 @@
+class Solution {
+    public String frequencySort(String s) {
+       
+       HashMap<Character,Integer> hm=new HashMap<>();
+        
+        for(int i=0;i<s.length();i++){
+            hm.put(s.charAt(i),hm.getOrDefault(s.charAt(i),0)+1);
+        }
+        
+        PriorityQueue<Pair> pq=new PriorityQueue<>(new sortComparator());
+        
+        for(Map.Entry<Character,Integer> e:hm.entrySet()){
+            pq.add(new Pair(e.getKey(),e.getValue()));
+        }
+        
+        String ans="";
+        
+        while(pq.size()>0){
+        Pair temp=pq.poll();
+        char ch=temp.getKey();
+        int k=temp.getVal();
+        while(k-->0){
+            ans=ans+""+ch;
+        }
+        }
+        
+        return ans;
+    }
+}
+
+class Pair{
+    char key;
+    int val;
+    
+   Pair(char key,int val){
+       this.key=key;
+       this.val=val;
+   }
+    
+    int getVal(){
+        return val;
+    }
+    
+    char getKey(){
+        return key;
+    }
+}
+
+class sortComparator implements Comparator<Pair>{
+    
+    public int compare(Pair a,Pair b){
+        if(a.getVal()>b.getVal()){
+            return -1;
+        }else if(a.getVal()<b.getVal()){
+            return 1;
+        }
+        return 0;
+    }
+}
