ef7fba94d044a5f886ed507648c7fc707c97ce61
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-12-08 12:21:10 +0530

    Time: 1 ms (77.58%), Space: 41.9 MB (52.43%) - LeetHub

---
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        // if(root1.val!=root2.val) return false;
+        
+        ArrayList<Integer> tree1=new ArrayList<>();
+        dfs(root1,tree1);
+        
+        ArrayList<Integer> tree2=new ArrayList<>();
+        dfs(root2,tree2);
+        
+        if(tree1.size()!=tree2.size()) return false;
+        
+        for(int i=0;i<tree1.size();i++){
+            if(tree1.get(i)!=tree2.get(i)) return false;
+        }
+        
+        return true;
+        
+    }
+    
+    private void dfs(TreeNode root, ArrayList<Integer> ans){
+        if(root==null) return;
+        
+        if(root.left==null && root.right==null) 
+        {
+            ans.add(root.val);
+            return;
+        }
+        
+       
+        dfs(root.left,ans);
+        dfs(root.right,ans);
+    }
+}
