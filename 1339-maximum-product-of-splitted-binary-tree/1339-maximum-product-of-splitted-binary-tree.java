138044a91d56dd2c866946e2703c8de51529dcdf
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-12-10 19:53:54 +0530

    Time: 31 ms (25.66%), Space: 80.6 MB (11.32%) - LeetHub

--- 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.java
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.java
@@ -15,39 +15,42 @@
  */
 class Solution {
     long totalSum;
-    
+    ArrayList<Long> arr=new ArrayList<>();
     public int maxProduct(TreeNode root) {
         
         long ans=0;
         totalSum=0;
         int mod=1000_000_007;
-        HashMap<TreeNode,Long> hm=new HashMap<>();
-        totalSum= dfs(root,0,hm);
+        // HashMap<TreeNode,Long> hm=new HashMap<>();
+        totalSum= dfs(root,arr);
        
         // System.out.println(totalSum+" *");
-        for(Map.Entry<TreeNode , Long> e:hm.entrySet()){
-            TreeNode node=e.getKey();
-            long currSum=e.getValue();
-            // System.out.println(node.val+" "+currSum);
-            ans=Math.max(ans,currSum*(totalSum-currSum));
-        }
+        // for(Map.Entry<TreeNode , Long> e:hm.entrySet()){
+        //     TreeNode node=e.getKey();
+        //     long currSum=e.getValue();
+        //     // System.out.println(node.val+" "+currSum);
+        //     ans=Math.max(ans,currSum*(totalSum-currSum));
+        // }
         
         // helper(root,hm,ans);
+        
+        for(int i=0;i<arr.size();i++){
+            ans=Math.max(ans,(totalSum-arr.get(i))*arr.get(i));
+        }
         
         return (int)((ans%mod));
     }
     
    
     
-    public long dfs(TreeNode root,long currSum,HashMap<TreeNode,Long> hm){
+    public long dfs(TreeNode root,ArrayList<Long> arr){
         if(root==null){
             return 0;
         }
         
         
-                  currSum=dfs(root.left,currSum,hm)+root.val+dfs(root.right,currSum,hm);
-        totalSum=currSum+totalSum;
-        hm.put(root,currSum);
+                  long currSum=dfs(root.left,arr)+root.val+dfs(root.right,arr);
+        arr.add(currSum);
         return currSum;
     }
 }
