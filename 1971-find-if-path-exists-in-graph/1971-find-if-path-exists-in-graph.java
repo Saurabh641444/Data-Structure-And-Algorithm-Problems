2882a39d08647e4a8c8418c3ac06919dd95cccfe
Saurabh Jejurkar <62086965+Saurabh641444@users.noreply.github.com>
2022-12-19 23:01:00 +0530

    Time: 30 ms (90.84%), Space: 139.2 MB (78.66%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,18 +1,65 @@
-class Solution {
-    public boolean validPath(int n, int[][] edges, int source, int destination) {
-        HashMap<Integer,ArrayList<Integer>> adj=new HashMap<>();
-        boolean vis[]=new boolean[n];
+class UnionFind{
+      
+    int parent[];
+    int rank[];
+    
+    UnionFind(int n){
+        parent=new int[n];
+        rank=new int[n];
         
-        for(int[] edge:edges){
-            int a=edge[0];
-            int b=edge[1];
-            
-            adj.computeIfAbsent(a,val-> new ArrayList<Integer>()).add(b);
-            adj.computeIfAbsent(b,val-> new ArrayList<Integer>()).add(a);
-
+        for(int i=0;i<n;i++){
+            parent[i]=i;
         }
         
-        return dfs(adj,vis,source,destination);
+    }
+    
+    int find(int x){
+        if(parent[x]==x) return x;
+        return parent[x]=find(parent[x]);
+    }
+    
+    void union(int x,int y){
+        int parentX=find(x);
+        int parentY=find(y);
+        
+        if(parentX!=parentY){
+            if(rank[parentX]>rank[parentY]){
+                parent[parentY]=parentX;
+            }else if(rank[parentX]<rank[parentY]){
+                parent[parentX]=parentY;
+            }else{
+                parent[parentY]=parentX;
+                rank[parentX]++;
+                
+            }
+        }
+    }
+      
+}
+
+class Solution {
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+//         HashMap<Integer,ArrayList<Integer>> adj=new HashMap<>();
+//         boolean vis[]=new boolean[n];
+        
+//         for(int[] edge:edges){
+//             int a=edge[0];
+//             int b=edge[1];
+            
+//             adj.computeIfAbsent(a,val-> new ArrayList<Integer>()).add(b);
+//             adj.computeIfAbsent(b,val-> new ArrayList<Integer>()).add(a);
+
+//         }
+        
+//         return dfs(adj,vis,source,destination);
+        
+           UnionFind obj=new UnionFind(n);
+        
+           for(int edge[]:edges){
+               obj.union(edge[0],edge[1]);
+           }
+        
+           return obj.find(source)==obj.find(destination);
     }
     
     private boolean dfs(HashMap<Integer,ArrayList<Integer>> adj,boolean[] vis,int curr,int dest){
